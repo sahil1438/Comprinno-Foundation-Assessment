5459c1aae309bb85dc848417c95334229644e56a
sahil1438 <85025768+sahil1438@users.noreply.github.com>
2021-05-30 13:27:00 +0530

    Add files via upload

---
+++ Beautiful.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Beautiful 
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
